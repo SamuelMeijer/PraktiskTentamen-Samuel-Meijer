0797e11ab38b94aed63ef2cf138dcaf266fefc23
Samuel Meijer <Samuel.j.meijer@gmail.com>
2020-12-11 11:34:57 +0100

    Adding prototype method 'showTime' for Country-constructor

--- js/app.js
+++ js/app.js
@@ -10,7 +10,6 @@
     - Skapa ett instansobjekt av Country för varje land du hämtar. 
     - Använd html-elementen för att presentera infon.
 
-
     För G:
     - Du har med alla punkter.
     - Appen fungerar utan error.
@@ -21,8 +20,6 @@
     - Tiden visas korrekt i samtliga fall 
     - Koden är strukturerad på ett sätt som är logiskt och lätt att följa. (Upplägg och namngivning)
     - Koden är tydligt kommenterad 
-
-
 */
 
 /* ***** ELEMENTS ***** */
@@ -30,6 +27,7 @@ const sectionElements = document.querySelectorAll('main section');
 
 /* ***** API ***** */
 const url = 'https://restcountries.eu/rest/v2/all'
+// const url = 'https://restcountries.eu/rest/v2/name/denmark' //TODO: REMOVE! TESTING TIMEZONES
 
 // TODO: Add try-catch when parsing from json
 // Adding filter to only include the name, flag and timezone of the countries in the response
@@ -38,20 +36,28 @@ fetch(`${url}?fields=name;flag;timezones`).then(response => response.json()).the
 /* Response-handling */
 function handleResponse (responseData) {
     // reponseData.length === 250 (0-249)
-    console.log(responseData.length);
+    // console.log(responseData.length);
 
     // console.log(responseData);
     for (let i = 0; i < 3; i++) {
+        //
         let randomNumber = Math.floor(Math.random() * responseData.length);
 
+        // TODO: FIX? If several timezones are available, the first(lowest UTC) timezone will always be used.
         let newCountry = new Country(responseData[randomNumber].flag, responseData[randomNumber].name, responseData[randomNumber].timezones[0]);
 
         console.log(newCountry);
-        
-        /* TODO: Adding the country to the DOM */
-        /* Showing the flag on the DOM */
-        // sectionElements[i].children[0].setAttribute('src', 'URL');
 
+        /* Adding the country to the DOM */
+        /* Showing the flag of the country on the DOM */
+        sectionElements[i].children[0].setAttribute('src', `${newCountry.flagUrl}`);
+
+        /* Showing the name of the country on the DOM */
+        sectionElements[i].children[1].textContent = `${newCountry.name}`;
+
+        /* Showing the current time in the country on the DOM */
+        sectionElements[i].children[2].textContent = newCountry.showTime();
+        // newCountry.showTime();
     }
 }
 
@@ -69,5 +75,52 @@ function Country (_flagUrl, _name, _timezone) {
 }
 
 Country.prototype.showTime = function () {
+    // Getting the current time in GMT+0100
+    let currentDate = new Date();
+    // Getting the current hours in UTC
+    let currentTimeUTC = currentDate.getUTCHours();
+    let currenTimeMinutes = currentDate.getMinutes();
+    // TODO: REMOVE! Used while developing
+    // console.log(currentDate);
+    // console.log(currentTimeUTC);
 
+    // Defining a variable that will be used to display the current hour in the local timezone after calculating difference from UTC
+    let localTimezoneHours = null;
+    // Creating a new string containing the hourly difference from UTC in the local timezone
+    let localUtcDifference = this.timezone.substr(4,2);
+
+    // Calculating the current hour in the local timezone.
+    switch(this.timezone[3]) {
+        case '+':
+            localTimezoneHours = currentTimeUTC + Number(localUtcDifference);
+            break;
+        case '-':
+            localTimezoneHours = currentTimeUTC - Number(localUtcDifference);
+            break;
+        default:
+            localTimezoneHours = currentTimeUTC;
+            break;
+    }
+
+    // Evaluating if the local timezone has enterd a new day or is one day behind
+    if (localTimezoneHours < 0) {
+        // If the localtimezone has a negative value, the country is still on the previous day
+        localTimezoneHours = 24 + localTimezoneHours;
+    } else if (localTimezoneHours > 24) {
+        // If the localtimezone has value greater than 24, the country has entered a new day
+        localTimezoneHours = localTimezoneHours - 24;
+    };
+
+    // Converting the local time from type Number into type String
+    let localTimezoneHoursStr = localTimezoneHours.toString();
+    // Evalutating if the string only contains one digit, adding a 0 before if it does.
+    if (localTimezoneHoursStr.length === 1) {
+        localTimezoneHoursStr = `0${localTimezoneHoursStr}`;
+    };
+
+    // Defining a new string containing both the hours and minutes of the local timezone HH:MM
+    let currentTime = `${localTimezoneHoursStr}:${currenTimeMinutes}`;
+
+    // Returning the string
+    return currentTime;
 }
