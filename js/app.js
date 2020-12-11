c329b0d74c8de891eddb76391862cf8c1aafb2a7
Samuel Meijer <Samuel.j.meijer@gmail.com>
2020-12-11 08:38:53 +0100

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,12 @@
+/* *** Krav ***
+    - En Country “mall” som ska innehålla
+    - En constructor med 3 parametrar och 3 egenskaper
+        - Namnet på landet
+        - Landets tidszon
+        - URL till landets flagga
+    - Minst 1 prototyp-metod
+        - Metoden ska returnera den nuvarande tiden i landet i det här formatet: tt:mm, tex 10:34. (Du behöver inte räkna med halvtimmar som finns i vissa tidszoner.)
+    - Hämta alla länder med restcountry API:et och välj ut 3 stycken random. 
+    - Skapa ett instansobjekt av Country för varje land du hämtar. 
+    - Använd html-elementen för att presentera infon.
+*/
