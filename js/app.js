7b9a363aa677be116cf2b4e65e7fe426af15f3cc
Samuel Meijer <Samuel.j.meijer@gmail.com>
2020-12-11 09:07:56 +0100

    Adding basic API-functionality

--- js/app.js
+++ js/app.js
@@ -9,4 +9,40 @@
     - Hämta alla länder med restcountry API:et och välj ut 3 stycken random. 
     - Skapa ett instansobjekt av Country för varje land du hämtar. 
     - Använd html-elementen för att presentera infon.
+
+
+    För G:
+    - Du har med alla punkter.
+    - Appen fungerar utan error.
+    - Tiden visas korrekt i de allra flesta fall
+
+    För VG:
+    - Du har använt arrays och for-loopar för att hantera Country instansobjekten
+    - Tiden visas korrekt i samtliga fall 
+    - Koden är strukturerad på ett sätt som är logiskt och lätt att följa. (Upplägg och namngivning)
+    - Koden är tydligt kommenterad 
+
+
 */
+
+
+/* ***** API ***** */
+const url = 'https://restcountries.eu/rest/v2/all'
+
+// TODO: Add try-catch when parsing from json
+// Adding filter to only include the name, flag and timezone of the countries in the response
+fetch(`${url}?fields=name;flag;timezones`).then(response => response.json()).then(handleResponse).catch(handleError);
+
+/* Response-handling */
+function handleResponse (responseData) {
+    // reponseData.length === 250 (0-249)
+    console.log(responseData.length);
+    console.log(responseData);
+}
+
+
+/* Error-handling */
+function handleError (err) {
+    // TODO: Improve!
+    console.error(err.message);
+};
